47b45602473e3373896590c97d494b31c470dca2
Laura <Laura@DESKTOP-EB15VVP.Home>
2021-09-22 08:59:33 +0200

    LAURA 22-09-2021 8:54 EjercHoja1 11 al 17 (no 12,16)

--- EjercHoja1/src/paquete/Ej11.java
+++ EjercHoja1/src/paquete/Ej11.java
@@ -9,7 +9,7 @@ public class Ej11 {
 		Programa que lee nombre y año de nacimiento de una serie de alumnos hasta introducir
 		como nombre de alumno “fin”. En ese momento mostrará la edad media de los alumnos y
 		el nombre del alumno menor. NOTA: Al introducir como nombre “fin”, no nos ha de pedir el
-		año de nacimiento.
+		año de nacimiento. 
 		*/
 		
 		int nacim;
